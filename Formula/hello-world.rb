76fedfa411238cf0b49a850f00b29e1460a77930
brendan <227456334+brendan-nasa@users.noreply.github.com>
2026-02-19 20:48:02 +0000

    hello-world: update 0.4_6 bottle.

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -8,9 +8,9 @@ class HelloWorld < Formula
 
   bottle do
     root_url "https://github.com/brendan-nasa/hello-world/releases/download/v0.4"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fa2175e0481200ccdb12857d4729711d6b65cd0380eae9b371511d85daab31e1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff1644f93873c0447474051b6e92f42e35de369176190f123fa9b6e3ad6a18e6"
-    sha256 cellar: :any_skip_relocation, sequoia:       "b33c1a926c3d92c404376b871a51a5f903cd902711c79254aeff3654dbc16293"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "292a63fd5abdc14bf41893be73226df9e18648e1cb62465371d882f8e4dbd80c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34332c45c53a842084b84ffaf6d5269a2d3b86773547eded21a669945995f373"
+    sha256 cellar: :any_skip_relocation, sequoia:       "799283c2a07b1589f4b45088490c487eaf95bc0ecc5b5f0a180836817da35c33"
   end
 
   depends_on "cmake" => :build
